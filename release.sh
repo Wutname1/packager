7d5827c834945872073d2ab3afae1bcf63e8be92
wutname1 <jeremy12@gmail.com>
2019-10-24 23:23:23 -0500

    Add travis build number & add support for git submodules

--- release.sh
+++ release.sh
@@ -958,6 +958,7 @@ escape_substr() {
 # Filter for simple repository keyword replacement.
 vcs_filter() {
 	sed \
+		-e "s/@travis-build-number@/$TRAVIS_BUILD_NUMBER/g" \
 		-e "s/@project-revision@/$si_project_revision/g" \
 		-e "s/@project-hash@/$si_project_hash/g" \
 		-e "s/@project-abbreviated-hash@/$si_project_abbreviated_hash/g" \
